eba26a2756d02d1debf570c9c917da5368948e96
GiggioG <dimitrstanimirov@gmail.com>
2024-07-29 12:49:57 +0300

    fix presenter.h

--- include/Presenter.h
+++ include/Presenter.h
@@ -17,7 +17,6 @@ public:
 
 	static void drawObject(SDL_Texture* texture);
 	static void drawObject(Drawable& drawable);
-	static void drawObject(Drawable& drawable, float angle, SDL_Point* pivot = NULL);
 	static void drawObject(DrawableSrcRect& drawableSrcRect);
 
 private:
@@ -28,16 +27,6 @@ template <typename Params>
 void drawObject(Params& value)
 {
 	Presenter::drawObject(value);
-}
-
-template <typename Param1, typename Param2>
-void drawObject(Param1& p1, Param2 p2){
-	Presenter::drawObject(p1, p2);
-}
-
-template <typename Param1, typename Param2, typename Param3>
-void drawObject(Param1& p1, Param2 p2, Param3 p3) {
-	Presenter::drawObject(p1, p2, p3);
 }
 
 SDL_Texture* loadTexture(string path);
